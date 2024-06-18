f83168624ce9d0c854effa64d1697f3b7cb1cd06
ruslangyurov <ruslangyrov85@outlook.com>
2024-06-18 14:42:03 +0100

    18.06.2024 tuesday 14:41

--- client/trailer-tracker/src/Components/LoginForm.js
+++ client/trailer-tracker/src/Components/LoginForm.js
@@ -2,38 +2,76 @@ import * as React from 'react';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import SendIcon from '@mui/icons-material/Send';
-import { useState } from "react";
+import { useState, useEffect} from "react";
+import { useNavigate } from "react-router-dom";
 import axios from '../apiAxios/axios';
+
+
+
 
 
 export const BasicLogin = () => {
   const[username, setUsername] = useState("")
   const[password, setPassword] = useState("")
+  const [errMsg, setErrMsg] = useState("")
+  const[isAuth, setIsAuth] = useState(false)
   const LOGIN_URL = '/auth'
-  const handleLogin = (e) => {
-    e.preventDefault();
-    axios.post(LOGIN_URL, {username, password}).then(response => {console.log(response.data); res.send(response.data)})
-    
 
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setErrMsg("")
+  },[username, password])
+
+  useEffect(() => {
+    if (isAuth) navigate('/')}, [isAuth])
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+    const res = await axios.post(LOGIN_URL, {username, password}).catch(err => {
+      if (!err?.response) {
+        setErrMsg("No Server Response");
+      } else if (err.response?.status === 409) {
+        setErrMsg("Username Taken");
+      } else if (err.response.status === 404) {
+        setErrMsg("Page not found");
+      } else if (err.response.status === 400) {
+        setErrMsg("All fields are required")
+      } else {
+        setErrMsg("Unknown error")
+      }
+    })
+    setUsername("")
+    setPassword("")
+    setIsAuth(true)
+  }
+     
+    catch (err) {
+        console.err(err)
+    }
   }
 
   return (
+   
    <div>
+        <h1>{errMsg}</h1>
         <TextField
           required
           type = "String"
           id="Username"
           label="Username"
-          defaultValue=""
+          value= {username}
           helperText="Please enter username"
           onChange={(e) => {setUsername(e.target.value)}}
+          
         />
         <TextField 
           required
           type = "Password"
           id="Password"
           label="Password"
-          defaultValue=""
+          value= {password}
           helperText="Please enter password"
           onChange={(e) => {setPassword(e.target.value)}}
         />
