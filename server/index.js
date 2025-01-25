10be8a1eaa0a6460d8db259e98a5b164bfe770de
ruslangyurov <157134878+ruslangyurov@users.noreply.github.com>
2025-01-25 23:36:24 +0000

    25/01/2025 Saturday 23:36

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ const app = express();
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 //import * as bodyParser from 'body-parser'
+import {connectDB} from './config'
 import { defaultRoute } from './routes/root.js';
 import {logger} from './middleware/logger.js';
 import yardRoute from './routes/yardRoute.js';
@@ -29,7 +30,7 @@ app.use(cors());
 app.use(cookieParser())
 const mongoDb = "mongodb+srv://ruslangyurov:UPhkK4FkI2nVFUii@oucluster.dqizjw9.mongodb.net/?retryWrites=true&w=majority"
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 const server = app.listen(PORT);
 const io = new Server(server);
 export default io;
