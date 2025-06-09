89e26a11572291964e385089e133269ea87aac72
ruslangyurov <157134878+ruslangyurov@users.noreply.github.com>
2025-06-09 01:49:11 +0000

    09/06/2025 Monday 02:49

--- server/index.js
+++ server/index.js
@@ -53,9 +53,9 @@ app.use(cookieParser())
 const mongoDb = "mongodb+srv://ruslangyurov:UPhkK4FkI2nVFUii@oucluster.dqizjw9.mongodb.net/?retryWrites=true&w=majority"
 
 //app.use('/', express.static(path.join(__dirname, 'public')))
-app.use(express.static(path.join(__dirname, "../client/build")));
+app.use(express.static(path.join(__dirname, "../client/trailer-tracker/build")));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+  res.sendFile(path.join(__dirname, "../client/trailer-tracker/build", "index.html"));
 });
 
 
