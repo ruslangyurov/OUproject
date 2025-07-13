fe19c34321e7e466d1285170478a9b93070ff62d
ruslangyurov <157134878+ruslangyurov@users.noreply.github.com>
2025-07-13 21:57:52 +0100

    setTrestle to false when bay is deleted

--- client/trailer-tracker/src/Components/BayForm.js
+++ client/trailer-tracker/src/Components/BayForm.js
@@ -44,7 +44,7 @@ useEffect(() => {
   // Reset form if bay was deleted
   if (bayDeleted?.status && bayDeleted.bayNumber === props.number) {
     setEmptyBay(true);
-
+    props.setTrestle(false)
     props.setFormData(bayDelete);
   }
 }, [bayDeleted, props.number, props.setFormData]);
