8b540a3eca51425693f4f74ff16b827669ac05f7
ruslangyurov <157134878+ruslangyurov@users.noreply.github.com>
2025-08-01 23:38:22 +0100

    change errMsg in loginForm

--- client/trailer-tracker/src/Components/LoginForm.js
+++ client/trailer-tracker/src/Components/LoginForm.js
@@ -61,11 +61,14 @@ export const BasicLogin = () => {
 
   return (
  <>   
-  <Typography color="error" sx={{ mb: 2 }}>
-        {errMsg}
-  </Typography>
+   {errMsg && (
+   <Typography color="error" sx={{ mt: '80px', mb: 2 }}>
+    {errMsg}
+   </Typography>
+   )}
+
    <Box sx={{ display:"flex", flexWrap:"wrap",mt:"130px", flexDirection: {xs:"column", sm:"column", md:"row", lg:"row"},justifyContent:"flex-start", alignContent:"center" }}>
-        <h1>{errMsg}</h1>
+
         <TextField sx={{ width: { xs: "80%", md: "200px" }}}
           required
           type = "String"
