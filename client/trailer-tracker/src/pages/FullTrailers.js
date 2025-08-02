f0cf109bb3f77e301658362734b72447d1ea5878
ruslangyurov <157134878+ruslangyurov@users.noreply.github.com>
2025-08-02 21:43:33 +0100

    syntax

--- client/trailer-tracker/src/pages/FullTrailers.js
+++ client/trailer-tracker/src/pages/FullTrailers.js
@@ -1,12 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import axiosInstanse from '../apiAxios/axios';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
 import {
   Table, TableBody, TableCell, TableContainer, TableHead, TableRow,
   Paper, Box, Typography, useMediaQuery
