79f60461c1c37d42088fdeedc2c08c3759e0e0ed
ruslangyurov <157134878+ruslangyurov@users.noreply.github.com>
2025-08-03 02:34:51 +0100

    add import statement

--- client/trailer-tracker/src/pages/Search.js
+++ client/trailer-tracker/src/pages/Search.js
@@ -3,13 +3,10 @@ import {useState, useEffect} from 'react'
 import {useLocation} from 'react-router-dom'
 import axiosInstanse from '../apiAxios/axios'
 import { setRef } from '@mui/material'
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+import {
+  Table, TableBody, TableCell, TableContainer,
+  TableHead, TableRow, Paper, Box, Typography
+} from '@mui/material';
 
 
 export const Search = () => {
