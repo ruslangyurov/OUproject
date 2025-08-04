37847f1623071a20512ae764e0097fa7a4dfedb8
ruslangyurov <157134878+ruslangyurov@users.noreply.github.com>
2025-08-04 04:33:19 +0100

    fix path

--- client/trailer-tracker/src/App.js
+++ client/trailer-tracker/src/App.js
@@ -24,7 +24,7 @@ import {ProtectedRoute} from './Components/ProtectedRoute';
 import { RequestInterceptor, ResponseInterceptor } from './apiAxios/axios';
 import "./App.css"; 
 import { Profile } from './Components/Profile';
-import { setupInterceptors } from './apiAxios/axios/setupInterceptors';
+import { setupInterceptors } from './apiAxios/axios';
 
 
 export function App() {
