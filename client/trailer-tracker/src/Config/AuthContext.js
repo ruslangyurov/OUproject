124e117562a02c05a28e559b38627d9708a7657a
ruslangyurov <157134878+ruslangyurov@users.noreply.github.com>
2025-08-06 04:41:50 +0100

    changes to jwt flow to prevent jwt expired error popping up and adding useffect for reloading page

--- client/trailer-tracker/src/Config/AuthContext.js
+++ client/trailer-tracker/src/Config/AuthContext.js
@@ -6,7 +6,23 @@ export const AuthContext = createContext();
 
 export const AuthContextProvider = ({ children }) => {
   const [auth, setAuth] = useState("");                 // this will contain the authentication token sent by the server
- 
+  
+  useEffect(() => {
+    const refreshToken = async () => {
+      try {
+        const res = await axiosInstance.get("/auth/refresh", { withCredentials: true });
+        setAuth(res.data.accessToken);
+      } catch (err) {
+        console.log("No valid refresh token found.");
+      }
+    };
+
+    if (!auth) {
+      refreshToken();
+    }
+  }, []);
+
+
   const contextValue = useMemo(() => {
     if (!auth) {
       return ({isAuth:false, auth, setAuth})
--- server/controller/userAuthController.js
+++ server/controller/userAuthController.js
@@ -35,7 +35,7 @@ const login = asyncHandler(async (req, res) => {
         },
         
         process.env.TOKEN_SECRET,
-        { expiresIn: '1m' }
+        { expiresIn: '10m' }
     )
 
     const refreshToken = jwt.sign(
