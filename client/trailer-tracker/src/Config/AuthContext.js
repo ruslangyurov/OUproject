c56cfecee891dd63fd6276a8e1a9bcdc3132dab1
ruslangyurov <157134878+ruslangyurov@users.noreply.github.com>
2025-08-06 04:48:11 +0100

    imports

--- client/trailer-tracker/src/Config/AuthContext.js
+++ client/trailer-tracker/src/Config/AuthContext.js
@@ -1,6 +1,7 @@
 import { createContext, useState, useContext } from "react";
 import {jwtDecode} from "jwt-decode";
-import { useMemo } from "react";
+import { useMemo, useEffect } from "react";
+import {axiosInstance} from '../apiAxios/axios';
 
 export const AuthContext = createContext();
 
