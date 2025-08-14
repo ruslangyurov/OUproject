e954e404011e4e1c6a2ec3d241b4b96fd5f13d0e
ruslangyurov <157134878+ruslangyurov@users.noreply.github.com>
2025-08-14 02:28:31 +0100

    import error

--- client/trailer-tracker/src/Components/Profile.js
+++ client/trailer-tracker/src/Components/Profile.js
@@ -6,6 +6,7 @@ import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
+import Button from '@mui/material/Button'
 import Paper from '@mui/material/Paper';
 import { Box, Tab } from "@mui/material";
 import EditUserInfo from "./EditUserInfo";
