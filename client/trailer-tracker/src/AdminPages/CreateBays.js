1e2af65bdbb45999c6b9dede9ad25a8b80402675
ruslangyurov <157134878+ruslangyurov@users.noreply.github.com>
2025-09-06 23:35:10 +0100

    create bay validation logic

--- client/trailer-tracker/src/AdminPages/CreateBays.js
+++ client/trailer-tracker/src/AdminPages/CreateBays.js
@@ -23,8 +23,8 @@ export const CreateBays = () => {
 
   const handleSubmitSingle = async(e) => {
     e.preventDefault();
-    const num = Number(number)
-    await axiosInstance.post(SINGLE_URL, { bayNumber: num }).then(() => setSuccessMsg("Bay successfully created"))
+   
+    await axiosInstance.post(SINGLE_URL, { bayNumber: number }).then(() => setSuccessMsg("Bay successfully created"))
     .catch((err) => setErrMsg(err.response?.data?.message || "An error occurred"));
   }
 
@@ -76,7 +76,7 @@ export const CreateBays = () => {
 
               // Only allow numbers 1 and above
               if (num >= 1) {
-                setNumber(value);
+                setNumber(Number(value));
               }
             }}
             onClick={() => {
