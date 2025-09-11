8353419d7916eb8b237455606067ab99754dbfd3
ruslangyurov <157134878+ruslangyurov@users.noreply.github.com>
2025-09-11 22:53:39 +0100

    syntax

--- server/controller/yardController.js
+++ server/controller/yardController.js
@@ -158,7 +158,7 @@ const getEmptyTrailers = async(req, res) => {
 
 const getFullTrailers = async(req, res) => {
 
-    const bays = await Bay.find({fullTrailer:"Full"}).exec()
+    const bays = await Bay.find({fullTrailer:true}).exec()
     if (!bays) {
         return res.status(400).json("No bays found")
     
