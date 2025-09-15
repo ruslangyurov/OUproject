bfb3a0a5c3e13b9ffe0d76ae09bdc71a90c13b96
ruslangyurov <157134878+ruslangyurov@users.noreply.github.com>
2025-09-15 02:17:36 +0100

    fix full trailers page

--- client/trailer-tracker/src/pages/FullTrailers.js
+++ client/trailer-tracker/src/pages/FullTrailers.js
@@ -76,7 +76,7 @@ export const FullTrailers = () => {
         </TableHead>
         <TableBody>
           {fullTrailers.map((bay) => (
-            <TableRow key={bay.number} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+            <TableRow key={bay.bayNumber} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
               <TableCell component="th" scope="row">{bay.bayNumber}</TableCell>
               <TableCell align="right">{bay.trailerNumber}</TableCell>
               <TableCell align="right">{bay.stockDelivered}</TableCell>
